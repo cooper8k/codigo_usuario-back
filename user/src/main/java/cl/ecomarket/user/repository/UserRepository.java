cf15b09374e8784783d1caf8103b0274e1157af4
cooper8k <ange.prado@duocuc.cl>
2025-05-15 23:30:27 -0400

    1.0.4

--- user/src/main/java/cl/ecomarket/user/repository/UserRepository.java
+++ user/src/main/java/cl/ecomarket/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package cl.ecomarket.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import cl.ecomarket.user.model.User;
@@ -12,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     // este medotodo busca por el nombre del usuario y devuelve una lista de usuarios
     List<User> findByName(String name);    
 
+    @Query("delete  from user where estado = false")
+    void deleteByEstadoFalse();
 }
